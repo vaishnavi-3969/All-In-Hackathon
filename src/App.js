10abff4084e037bbbfa8b43323b0d09cd49d8bd9
Vaishnavi Kale <vaishnavi.kale3011@gmail.com>
2023-10-22 00:24:02 +0530

    Revert "new"

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Auth0Provider } from '@auth0/auth0-react';
 import Homepage from "./components/Homepage";
-import EmployeeHomepage from "./components/employee/EmployeeHomepage";
-import EmployeerHomepage from "./components/employeer/EmployeerHomepage";
-import EmployeerProfile from "./components/employeer/EmployeerProfile";
-import EmployeeProfile from "./components/employee/EmployeeProfile";
 
 function App() {
 
-  const domain = "dev-8io25ngx3xtfx5n2.us.auth0.com"
-  const clientId = "JvjXdMrJMnGWtJNqsG5AKNzeNBkWhQNm"
+  const domain="dev-8io25ngx3xtfx5n2.us.auth0.com"
+  const clientId="JvjXdMrJMnGWtJNqsG5AKNzeNBkWhQNm"
 
   return (
     <div>
@@ -20,11 +16,7 @@ function App() {
           redirectUri={window.location.origin}
         >
           <Routes>
-            <Route path="/" element={<Homepage />} exact />
-            <Route path="/employee_homepage" element={<EmployeeHomepage />} exact />
-            <Route path="/employeer_homepage" element={<EmployeerHomepage />} exact />
-            <Route path="/employee_profile" element={<EmployeeProfile />} exact />
-            <Route path="/employeer_profile" element={<EmployeerProfile />} exact />
+            <Route path="/" element={<Homepage />} />
           </Routes>
         </Auth0Provider>
       </BrowserRouter>

53f4cd13d97c4dca2af13655808d976f535c1ebb
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-10-22 05:12:47 +0530

    minor changes

--- src/components/employee/EmployeeNavbar.js
+++ src/components/employee/EmployeeNavbar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { FiUser, FiBriefcase, FiInfo } from 'react-icons/fi';
+import Logo from '../../assets/logo.png';
+import { Link } from 'react-router-dom';
 
 const EmployeeNavbar = () => {
     return (
@@ -8,18 +10,25 @@ const EmployeeNavbar = () => {
             initial={{ opacity: 0, x: -100 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.5 }}
-            className="bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 p-4 shadow-lg"
+            className="bg-gray-900 p-4 shadow-lg"
         >
             <div className="flex items-center justify-between text-white">
                 <div className="flex items-center space-x-4">
+                    <div>
+                        <Link to="/employee_homepage">
+                            <img src={Logo} alt='' width={50} />
+                        </Link>
+                    </div>
+                    <Link to="/employee_homepage">
                     <motion.h1
                         initial={{ x: -100 }}
                         animate={{ x: 0 }}
                         transition={{ duration: 0.5 }}
                         className="text-2xl font-semibold"
                     >
-                        EmployeeNavbar
+                        EquiHire
                     </motion.h1>
+                    </Link>
                 </div>
                 <div className="flex items-center space-x-6">
                     <motion.div
--- src/components/employer/EmployerNavbar.js
+++ src/components/employer/EmployerNavbar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { FiUser, FiBriefcase, FiInfo } from 'react-icons/fi';
+import Logo from '../../assets/logo.png';
+import { Link } from 'react-router-dom';
 
 const EmployerNavbar = () => {
     return (
@@ -8,18 +10,23 @@ const EmployerNavbar = () => {
             initial={{ opacity: 0, x: -100 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.5 }}
-            className="bg-gradient-to-r from-green-400 via-green-500 to-green-600 p-4 shadow-lg"
+            className="bg-gray-900 p-4 shadow-lg"
         >
             <div className="flex items-center justify-between text-white">
                 <div className="flex items-center space-x-4">
-                    <motion.h1
-                        initial={{ x: -100 }}
-                        animate={{ x: 0 }}
-                        transition={{ duration: 0.5 }}
-                        className="text-2xl font-semibold"
-                    >
-                        EmployerNavbar
-                    </motion.h1>
+                    <Link to="/employer_homepage">
+                        <img src={Logo} alt='' width={50} />
+                    </Link>
+                    <Link to="/employer_homepage">
+                        <motion.h1
+                            initial={{ x: -100 }}
+                            animate={{ x: 0 }}
+                            transition={{ duration: 0.5 }}
+                            className="text-2xl font-semibold"
+                        >
+                            EquiHire
+                        </motion.h1>
+                    </Link>
                 </div>
                 <div className="flex items-center space-x-6">
                     <motion.div

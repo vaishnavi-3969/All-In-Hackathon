aa765ad28a22583b20af660ff4fc46e91c8785ea
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-10-22 00:12:18 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./components/Homepage";
+import EmployeeHomepage from "./components/employee/EmployeeHomepage";
+import EmployeerHomepage from "./components/employeer/EmployeerHomepage";
 
 function App() {
   return (
@@ -7,6 +9,8 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage/>} exact/>
+        <Route path="/employee_homepage" element={<EmployeeHomepage/>} exact/>
+        <Route path="/employeer_homepage" element={<EmployeerHomepage/>} exact/>
       </Routes>
       </BrowserRouter>
     </div>

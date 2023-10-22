f072403015e7ca5fae6549fe3d2c746ec82991fa
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-10-22 10:21:27 +0530

    dasboard

--- src/components/employer/Dashboard.js
+++ src/components/employer/Dashboard.js
@@ -1,8 +1,129 @@
+import React, { useState, useEffect } from 'react';
+import { getDatabase, ref, onValue } from 'firebase/database';
+import { useAuth0 } from '@auth0/auth0-react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faBuilding,
+    faUser,
+    faClipboardList,
+    faBullhorn,
+    faCheckCircle,
+    faChartBar,
+} from '@fortawesome/free-solid-svg-icons';
+import { motion } from 'framer-motion';
+import { app } from '../db/Firebase';
+import Calendar from 'react-calendar';
+
 const DashboardTab = () => {
+    const db = getDatabase(app);
+    const { user, isAuthenticated } = useAuth0();
+    const companyProfilesRef = ref(db, 'companies');
+    const [companyProfilesCount, setCompanyProfilesCount] = useState(0);
+    const [insights] = useState([
+        {
+            icon: faBullhorn,
+            text: 'Get noticed! Promote your job listings for increased visibility.',
+            number: 350,
+        },
+        {
+            icon: faCheckCircle,
+            text: 'Track your job posting success with detailed analytics.',
+            number: 6500,
+        },
+        {
+            icon: faChartBar,
+            text: 'Optimize your job descriptions for better results.',
+            number: 15,
+        },
+    ]);
+
+    useEffect(() => {
+        onValue(companyProfilesRef, (snapshot) => {
+            if (snapshot.exists()) {
+                const profiles = snapshot.val();
+                const count = Object.keys(profiles).length;
+                setCompanyProfilesCount(count);
+            }
+        });
+    }, [companyProfilesRef]);
+
+    const candidates = [
+        { status: 'Pending for Interview', count: 5 },
+        { status: 'Pending for Review', count: 10 },
+        { status: 'Meetings Today', count: 3 },
+    ];
+
     return (
         <div>
             <h2 className="text-2xl font-semibold mb-4">Dashboard</h2>
-            <p>Welcome to your EquiHire employer dashboard. Here you can manage your job listings, company profile, and more. Your contributions are making the job market more inclusive.</p>
+            <motion.p
+                initial={{ opacity: 0, y: 50 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5 }}
+                className="mb-4"
+            >
+                {isAuthenticated ? `Welcome, ${user.name}! ` : 'Welcome to your EquiHire employer dashboard. '}Here you can manage your job listings, company profile, and more. Your contributions are making the job market more inclusive.
+            </motion.p>
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                <motion.div
+                    initial={{ opacity: 0, scale: 0.5 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 0.5 }}
+                    className="bg-white p-4 rounded-lg shadow-md flex items-center"
+                >
+                    <FontAwesomeIcon icon={faBuilding} className="text-blue-500 text-3xl" />
+                    <div className="ml-4">
+                        <h3 className="font-semibold text-xl">Company Profiles</h3>
+                        <p className="text-gray-600">{companyProfilesCount} profiles</p>
+                    </div>
+                </motion.div>
+
+                {insights.map((insight, index) => (
+                    <motion.div
+                        key={index}
+                        initial={{ opacity: 0, scale: 0.5 }}
+                        animate={{ opacity: 1, scale: 1 }}
+                        transition={{ duration: 0.5, delay: index * 0.2 }}
+                        className="bg-white p-4 rounded-lg shadow-md flex items-center"
+                    >
+                        <FontAwesomeIcon icon={insight.icon} className="text-blue-500 text-3xl" />
+                        <div className="ml-4">
+                            <p className="text-gray-600">
+                                {insight.text} ({insight.number})
+                            </p>
+                        </div>
+                    </motion.div>
+                ))}
+            </div>
+
+            <div className="mt-8">
+                <h3 className="text-xl font-semibold mb-4">Candidate Status</h3>
+                <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                    {candidates.map((candidate, index) => (
+                        <motion.div
+                            key={index}
+                            initial={{ opacity: 0, scale: 0.5 }}
+                            animate={{ opacity: 1, scale: 1 }}
+                            transition={{ duration: 0.5, delay: index * 0.2 }}
+                            className="bg-white p-4 rounded-lg shadow-md flex items-center"
+                        >
+                            <FontAwesomeIcon icon={faClipboardList} className="text-blue-500 text-3xl" />
+                            <div className="ml-4">
+                                <h3 className="font-semibold text-lg">{candidate.status}</h3>
+                                <p className="text-gray-600">{candidate.count} candidates</p>
+                            </div>
+                        </motion.div>
+                    ))}
+                </div>
+            </div>
+
+            <div className="mt-8">
+                <h3 className="text-xl font-semibold mb-4">Today's Meetings</h3>
+                <div className="bg-white p-4 rounded-lg shadow-md">
+                    <Calendar />
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/employer/EmployerProfile.js
+++ src/components/employer/EmployerProfile.js
@@ -1,9 +1,101 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { getDatabase, ref, set, onValue } from 'firebase/database';
+import { useAuth0 } from '@auth0/auth0-react';
+import { app } from '../db/Firebase';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+const validationSchema = Yup.object().shape({
+    companyName: Yup.string().required('Company Name is required'),
+    companyDescription: Yup.string().required('Company Description is required'),
+    companySize: Yup.string().required('Company Size is required'),
+    industry: Yup.string().required('Industry is required'),
+    officialWebsite: Yup.string().url('Invalid URL format'),
+    diversity: Yup.string(),
+    workplaceCulture: Yup.string(),
+    isDisabledFriendly: Yup.boolean(),
+    disabledFacilities: Yup.object().shape({
+        wheelchairAccess: Yup.boolean(),
+        accommodations: Yup.boolean(),
+        inclusiveSalary: Yup.boolean(),
+        other: Yup.string(),
+    }),
+});
+
+const initialValues = {
+    companyName: '',
+    companyDescription: '',
+    companySize: '',
+    industry: '',
+    officialWebsite: '',
+    diversity: '',
+    workplaceCulture: '',
+    isDisabledFriendly: false,
+    disabledFacilities: {
+        wheelchairAccess: false,
+        accommodations: false,
+        inclusiveSalary: false,
+        other: '',
+    },
+};
 
 const EmployerProfile = () => {
-  return (
-    <div>EmployerProfile</div>
-  )
-}
+    const db = getDatabase(app);
+    const { user } = useAuth0();
+    const userRef = ref(db, 'employerProfiles');
 
-export default EmployerProfile
+    useEffect(() => {
+        onValue(userRef, (snapshot) => {
+            const profileData = snapshot.val();
+            if (profileData) {
+                // Load profile data into your form or state if needed.
+            }
+        });
+    }, [userRef]);
+
+    const handleSaveProfile = (values) => {
+        const profileData = {
+            addedBy: user.email,
+            ...values,
+        };
+        set(userRef, profileData);
+    };
+
+    return (
+        <div>
+            <h2 className="text-2xl font-semibold mb-4">Employer Profile</h2>
+            <Formik
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+                onSubmit={(values, { resetForm }) => {
+                    handleSaveProfile(values);
+                    resetForm();
+                }}
+            >
+                {({ values }) => (
+                    <Form>
+                        {/* Add input fields for all your profile information */}
+                        <div className="mb-4">
+                            <label htmlFor="companyName">Company Name</label>
+                            <Field
+                                type="text"
+                                id="companyName"
+                                name="companyName"
+                                className="border p-2 rounded"
+                            />
+                            <ErrorMessage name="companyName" component="div" className="text-red-500" />
+                        </div>
+
+                        {/* ... Add more input fields here for other profile information ... */}
+
+                        <button type="submit" className="bg-blue-500 text-white p-2 rounded-lg">
+                            Save Profile
+                        </button>
+                    </Form>
+                )}
+            </Formik>
+        </div>
+    );
+};
+
+export default EmployerProfile;

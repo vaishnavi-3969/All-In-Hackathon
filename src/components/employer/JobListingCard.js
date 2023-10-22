75f535969872fb93b799af133e6c8bbdbcc20d9f
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-10-22 12:12:46 +0530

    job listings

--- src/components/employer/JobListingCard.js
+++ src/components/employer/JobListingCard.js
@@ -1,10 +1,79 @@
-const JobListingCard = ({ title, location, description }) => {
+import React, { useState, useEffect } from 'react';
+
+const JobListingCard = ({ title, location, description, website, genderInclusivity, disabilityInclusivity, accommodation, date }) => {
+    const [expanded, setExpanded] = useState(false);
+    const [randomLogo, setRandomLogo] = useState('');
+    const [candidatesApplied, setCandidatesApplied] = useState(0);
+    const [perfectMatches, setPerfectMatches] = useState(0);
+
+    useEffect(() => {
+        fetch('https://source.unsplash.com/200x150/?company')
+            .then((response) => {
+                setRandomLogo(response.url);
+            });
+
+        setCandidatesApplied(Math.floor(Math.random() * 100));
+        setPerfectMatches(Math.floor(Math.random() * 10));
+    }, []);
+
+    const toggleExpandedView = () => {
+        setExpanded(!expanded);
+    };
+
+    const handleApplyClick = () => {
+        alert(`Checking for relevant candidate match for ${title}`);
+    };
+
     return (
-        <div className="border p-4 rounded-lg shadow-md">
-            <h3 className="text-lg font-semibold">{title}</h3>
-            <p className="text-gray-600">{location}</p>
+        <div className={`border p-4 rounded-lg shadow-md cursor-pointer ${expanded ? 'bg-gray-100' : ''}`} onClick={toggleExpandedView}>
+            <div className="flex justify-between items-center">
+                <div>
+                    <img src={randomLogo} alt="Company Logo" className="p-1 w-50 h-25 md:rounded object-cover" />
+                </div>
+                <div>
+                    <h3 className="text-lg font-semibold">{title}</h3>
+                    <p className="text-gray-600">{location}</p>
+                </div>
+            </div>
             <p className="mt-2">{description}</p>
+            {expanded && (
+                <div className="mt-4">
+                    <div className="flex justify-between">
+                        <div>
+                            <p className="font-semibold">Website:</p>
+                            <a href={website} target="_blank" rel="noopener noreferrer" className="text-blue-500">{website}</a>
+                        </div>
+                        <div>
+                            <p className="font-semibold">Gender Inclusivity:</p>
+                            <p>{genderInclusivity}</p>
+                        </div>
+                    </div>
+                    <div className="flex justify-between mt-2">
+                        <div>
+                            <p className="font-semibold">Disability Inclusivity:</p>
+                            <p>{disabilityInclusivity}</p>
+                        </div>
+                        <div>
+                            <p className="font-semibold">Accommodation Available:</p>
+                            <p>{accommodation}</p>
+                        </div>
+                    </div>
+                    <div className="mt-4">
+                        <p className="font-semibold">Candidates Applied:</p>
+                        <p>{candidatesApplied}</p>
+                    </div>
+                    <div className="mt-2">
+                        <p className="font-semibold">Perfect Matches Found:</p>
+                        <p>{perfectMatches}</p>
+                    </div>
+                    <button onClick={handleApplyClick} className="mt-4 bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-700 cursor-pointer">
+                        Preview
+                    </button>
+                    <p>Posted on {date}</p>
+                </div>
+            )}
         </div>
     );
 };
+
 export default JobListingCard;
--- src/components/employer/JobListingsTab.js
+++ src/components/employer/JobListingsTab.js
@@ -1,26 +1,152 @@
-import JobListingCard from "./JobListingCard";
+import React from 'react';
+import JobListingCard from './JobListingCard';
 
 const JobListingsTab = () => {
+    const jobListings = [
+        {
+            title: 'Frontend Developer',
+            location: 'New York, NY',
+            description: 'We are looking for a skilled Frontend Developer to join our team. Apply now!',
+            logo: 'frontend-logo.png',
+            website: 'frontend',
+            genderInclusivity: 'Open to All Genders',
+            disabilityInclusivity: 'Accessible Workplace',
+            accommodation: 'Accommodation Available',
+            date: 'October 20, 2023',
+        },
+        {
+            title: 'Backend Developer',
+            location: 'San Francisco, CA',
+            description: 'Join our Backend Developer team and work on exciting projects.',
+            logo: 'backend-logo.png',
+            website: 'backend',
+            genderInclusivity: 'Open to All Genders',
+            disabilityInclusivity: 'Accessible Workplace',
+            accommodation: 'Accommodation Available',
+            date: 'October 21, 2023',
+        },
+        {
+            title: 'UX Designer',
+            location: 'Los Angeles, CA',
+            description: 'Passionate about user experience design? Join us!',
+            logo: 'ux-logo.png',
+            website: 'ux',
+            genderInclusivity: 'Open to All Genders',
+            disabilityInclusivity: 'Accessible Workplace',
+            accommodation: 'Accommodation Available',
+            date: 'October 22, 2023',
+        },
+        {
+            title: 'Product Manager',
+            location: 'Chicago, IL',
+            description: 'Seeking an experienced Product Manager to lead product development.',
+            logo: 'product-logo.png',
+            website: 'product',
+            genderInclusivity: 'Open to All Genders',
+            disabilityInclusivity: 'Accessible Workplace',
+            accommodation: 'Accommodation Available',
+            date: 'October 23, 2023',
+        },
+        {
+            title: 'Data Analyst',
+            location: 'Austin, TX',
+            description: 'Join our data analytics team and help drive data-driven decisions.',
+            logo: 'data-logo.png',
+            website: 'data',
+            genderInclusivity: 'Open to All Genders',
+            disabilityInclusivity: 'Accessible Workplace',
+            accommodation: 'Accommodation Available',
+            date: 'October 24, 2023',
+        },
+        {
+            title: 'Marketing Specialist',
+            location: 'Seattle, WA',
+            description: 'Exciting opportunity for a Marketing Specialist to drive brand awareness.',
+            logo: 'marketing-logo.png',
+            website: 'marketing',
+            genderInclusivity: 'Open to All Genders',
+            disabilityInclusivity: 'Accessible Workplace',
+            accommodation: 'Accommodation Available',
+            date: 'October 25, 2023',
+        },
+        {
+            title: 'HR Manager',
+            location: 'Denver, CO',
+            description: 'We are hiring an HR Manager to foster an inclusive workplace.',
+            logo: 'hr-logo.png',
+            website: 'hr',
+            genderInclusivity: 'Open to All Genders',
+            disabilityInclusivity: 'Accessible Workplace',
+            accommodation: 'Accommodation Available',
+            date: 'October 26, 2023',
+        },
+        {
+            title: 'Graphic Designer',
+            location: 'Boston, MA',
+            description: 'Join our design team and create visually stunning graphics.',
+            logo: 'graphic-logo.png',
+            website: 'graphic',
+            genderInclusivity: 'Open to All Genders',
+            disabilityInclusivity: 'Accessible Workplace',
+            accommodation: 'Accommodation Available',
+            date: 'October 27, 2023',
+        },
+        {
+            title: 'Content Writer',
+            location: 'Miami, FL',
+            description: 'Passionate about writing? Join us as a Content Writer!',
+            logo: 'content-logo.png',
+            website: 'content',
+            genderInclusivity: 'Open to All Genders',
+            disabilityInclusivity: 'Accessible Workplace',
+            accommodation: 'Accommodation Available',
+            date: 'October 28, 2023',
+        },
+        {
+            title: 'Sales Representative',
+            location: 'Houston, TX',
+            description: 'Exciting sales role to drive revenue and growth.',
+            logo: 'sales-logo.png',
+            website: 'sales',
+            genderInclusivity: 'Open to All Genders',
+            disabilityInclusivity: 'Accessible Workplace',
+            accommodation: 'Accommodation Available',
+            date: 'October 29, 2023',
+        },
+        {
+            title: 'Software Engineer',
+            location: 'San Diego, CA',
+            description: 'Join our software engineering team to build cutting-edge solutions.',
+            logo: 'software-logo.png',
+            website: 'software',
+            genderInclusivity: 'Open to All Genders',
+            disabilityInclusivity: 'Accessible Workplace',
+            accommodation: 'Accommodation Available',
+            date: 'October 30, 2023',
+        },
+    ];
+
     return (
         <div>
-            <h2 className="text-2xl font-semibold mb-4">Job Listings</h2>
-            <p>View, edit, or add new job listings to attract diverse talents. Let's build an inclusive workforce together!</p>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-                <JobListingCard
-                    title="Frontend Developer"
-                    location="New York, NY"
-                    description="We are looking for a skilled Frontend Developer to join our team. Apply now!"
-                />
-                <JobListingCard
-                    title="Backend Developer"
-                    location="San Francisco, CA"
-                    description="Join our Backend Developer team and work on exciting projects."
-                />
-                <JobListingCard
-                    title="UX Designer"
-                    location="Los Angeles, CA"
-                    description="Passionate about user experience design? Join us!"
-                />
+            <h2 className="text-3xl font-semibold mb-8">Explore Job Listings</h2>
+            <p className="text-lg text-gray-600 mb-8">
+                All Job Listings by your companies
+            </p>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+                {jobListings.map((job, index) => (
+                    <JobListingCard
+                        key={index}
+                        title={job.title}
+                        location={job.location}
+                        description={job.description}
+                        logo={job.logo}
+                        website={job.website}
+                        genderInclusivity={job.genderInclusivity}
+                        disabilityInclusivity={job.disabilityInclusivity}
+                        accommodation={job.accommodation}
+                        date={job.date}
+                    />
+                ))}
             </div>
         </div>
     );

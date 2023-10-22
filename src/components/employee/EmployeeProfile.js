a9cbbee40b2ad7cb97210e43f215df88b5daa83d
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-10-22 13:48:36 +0530

    js profile

--- src/components/employee/EmployeeProfile.js
+++ src/components/employee/EmployeeProfile.js
@@ -1,191 +1,136 @@
 import React, { useEffect, useState } from 'react';
-import { getDatabase, ref, get, set } from 'firebase/database';
 import { useAuth0 } from '@auth0/auth0-react';
+import { doc, getDoc, setDoc } from 'firebase/firestore';
 import { db } from '../db/Firebase';
 import EmployeeNavbar from './EmployeeNavbar';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 const EmployeeProfile = () => {
-    const [profileData, setProfileData] = useState(null);
-    const [activeSection, setActiveSection] = useState('basicInfo');
     const { user } = useAuth0();
+    const [profileData, setProfileData] = useState(null); 
+    const [activeSection, setActiveSection] = useState('basicInfo');
+
     useEffect(() => {
         const fetchUserProfile = async () => {
-            const userEmail = user.email;
+            const userRef = doc(db, 'employee_profiles', user.sub);
 
             try {
-                const userRef = ref(db, 'employee_profiles');
-                const snapshot = await get(userRef);
-
-                snapshot.forEach((childSnapshot) => {
-                    const employeeData = childSnapshot.val();
-
-                    if (employeeData.email === userEmail) {
-                        setProfileData(employeeData);
-                    }
-                });
+                const docSnapshot = await getDoc(userRef);
+                if (docSnapshot.exists()) {
+                    setProfileData(docSnapshot.data());
+                }
             } catch (error) {
                 console.error('Error fetching user profile:', error);
             }
         };
 
         fetchUserProfile();
-    }, [user]);
-
-    const saveProfileData = (updatedData) => {
-        // Update the profile data in Firebase
-        const userRef = ref(db, 'employee_profiles');
-        set(userRef, updatedData);
-
-        // Update the local state with the changes
-        setProfileData(updatedData);
-    };
+    }, [user.sub]);
 
     return (
         <div>
             <EmployeeNavbar />
             <div className="p-8 bg-white shadow-lg text-black">
-                <div className='min-h-screen w-1/4 bg-gray-100 drop-shadow rounded-lg'>
-                    <div className="p-4">
+                <div className="flex">
+                    <div className="w-1/4 bg-gray-100 p-4 rounded-l">
                         <h2 className="text-2xl font-bold text-blue-700 mb-4">User Profile</h2>
                         <ul>
                             <li
                                 onClick={() => setActiveSection('basicInfo')}
-                                className={`cursor-pointer ${activeSection === 'basicInfo' && 'text-blue-700'}`}
+                                className={`cursor-pointer ${
+                                    activeSection === 'basicInfo' && 'text-blue-700'
+                                }`}
                             >
                                 Basic Info
                             </li>
-                            <li
-                                onClick={() => setActiveSection('employmentHistory')}
-                                className={`cursor-pointer ${activeSection === 'employmentHistory' && 'text-blue-700'}`}
-                            >
-                                Employment History
-                            </li>
-                            <li
-                                onClick={() => setActiveSection('moreInfo')}
-                                className={`cursor-pointer ${activeSection === 'moreInfo' && 'text-blue-700'}`}
-                            >
-                                More Info
-                            </li>
                         </ul>
                     </div>
-                </div>
-
-                <div className="w-3/4 p-4">
-                    {activeSection === 'basicInfo' && (
-                        <BasicInfoSection
-                            profileData={profileData}
-                            saveProfileData={saveProfileData}
-                        />
-                    )}
-
-                    {activeSection === 'employmentHistory' && (
-                        <EmploymentHistorySection
-                            profileData={profileData}
-                            saveProfileData={saveProfileData}
-                        />
-                    )}
-
-                    {activeSection === 'moreInfo' && (
-                        <MoreInfoSection
-                            profileData={profileData}
-                            saveProfileData={saveProfileData}
-                        />
-                    )}
+                    <div className="w-3/4 p-4 rounded-r">
+                        {activeSection === 'basicInfo' && (
+                            <BasicInfoSection profileData={profileData} />
+                        )}
+                    </div>
                 </div>
             </div>
         </div>
     );
 };
 
-const BasicInfoSection = ({ profileData, saveProfileData }) => {
-    const [basicInfo, setBasicInfo] = useState(profileData && profileData.basicInfo);
-
-    const handleSave = () => {
-        const updatedData = {
-            ...profileData,
-            basicInfo: basicInfo,
-        };
-        saveProfileData(updatedData);
-    };
+const BasicInfoSection = ({ profileData }) => {
+    const { user } = useAuth0();
 
     return (
         <div>
-            {/* Add input fields for basic info and a Save button */}
-            <input
-                type="text"
-                placeholder="Name"
-                value={basicInfo.name}
-                onChange={(e) => setBasicInfo({ ...basicInfo, name: e.target.value })}
-            />
-            <input
-                type="text"
-                placeholder="Email"
-                value={basicInfo.email}
-                onChange={(e) => setBasicInfo({ ...basicInfo, email: e.target.value })}
-            />
-            <button onClick={handleSave}>Save</button>
-        </div>
-    );
-};
+            <h3 className="text-2xl font-semibold mb-4">Basic Info</h3>
+            <Formik
+                initialValues={{
+                    name: profileData?.basicInfo?.name || '',
+                    email: user.email || '',
+                    age: profileData?.basicInfo?.age || '',
+                    gender: profileData?.basicInfo?.gender || '',
+                }}
+                validationSchema={Yup.object({
+                    name: Yup.string().required('Name is required'),
+                    email: Yup.string().email('Invalid email address').required('Email is required'),
+                    age: Yup.number()
+                        .typeError('Age must be a number')
+                        .positive('Age must be a positive number')
+                        .integer('Age must be an integer'),
+                    gender: Yup.string().required('Gender is required'),
+                })}
+                onSubmit={async (values) => {
+                    const updatedProfileData = {
+                        ...profileData,
+                        basicInfo: values,
+                    };
 
-const EmploymentHistorySection = ({ profileData, saveProfileData }) => {
-    const [employmentHistory, setEmploymentHistory] = useState(profileData && profileData.employmentHistory);
-
-    const handleSave = () => {
-        const updatedData = {
-            ...profileData,
-            employmentHistory: employmentHistory,
-        };
-        saveProfileData(updatedData);
-    };
-
-    return (
-        <div>
-            {/* Add input fields for employment history and a Save button */}
-            <input
-                type="text"
-                placeholder="Company"
-                value={employmentHistory.company}
-                onChange={(e) => setEmploymentHistory({ ...employmentHistory, company: e.target.value })}
-            />
-            <input
-                type="text"
-                placeholder="Position"
-                value={employmentHistory.position}
-                onChange={(e) => setEmploymentHistory({ ...employmentHistory, position: e.target.value })}
-            />
-            <button onClick={handleSave}>Save</button>
-        </div>
-    );
-};
-
-const MoreInfoSection = ({ profileData, saveProfileData }) => {
-    const [moreInfo, setMoreInfo] = useState(profileData && profileData.moreInfo);
-
-    const handleSave = () => {
-        const updatedData = {
-            ...profileData,
-            moreInfo: moreInfo,
-        };
-        saveProfileData(updatedData);
-    };
-
-    return (
-        <div>
-            {/* Add input fields for more info and a Save button */}
-            <input
-                type="text"
-                placeholder="Education"
-                value={moreInfo.education}
-                onChange={(e) => setMoreInfo({ ...moreInfo, education: e.target.value })}
-            />
-            <input
-                type="text"
-                placeholder="Skills"
-                value={moreInfo.skills}
-                onChange={(e) => setMoreInfo({ ...moreInfo, skills: e.target.value })}
-            />
-            <button onClick={handleSave}>Save</button>
+                    const userRef = doc(db, 'employee_profiles', user.sub);
+                    await setDoc(userRef, updatedProfileData);
+                }}
+            >
+                <Form>
+                    <div className="mb-4">
+                        <label htmlFor="name">Name</label>
+                        <Field type="text" id="name" name="name" className="border rounded w-full p-2" />
+                        <ErrorMessage name="name" component="div" className="text-red-500" />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="email">Email</label>
+                        <Field
+                            type="email"
+                            id="email"
+                            name="email"
+                            className="border rounded w-full p-2"
+                            disabled
+                        />
+                        <ErrorMessage name="email" component="div" className="text-red-500" />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="age">Age</label>
+                        <Field type="number" id="age" name="age" className="border rounded w-full p-2" />
+                        <ErrorMessage name="age" component="div" className="text-red-500" />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="gender">Gender</label>
+                        <Field as="select" id="gender" name="gender" className="border rounded w-full p-2">
+                            <option value="" disabled>
+                                Select a gender
+                            </option>
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                            <option value="other">Other</option>
+                        </Field>
+                        <ErrorMessage name="gender" component="div" className="text-red-500" />
+                    </div>
+                    <button
+                        type="submit"
+                        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+                    >
+                        Save
+                    </button>
+                </Form>
+            </Formik>
         </div>
     );
 };

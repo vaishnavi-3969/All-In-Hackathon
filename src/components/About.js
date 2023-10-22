e6f55a0ef04fcd85381ce9df4e1a7828d9eefba5
Aryan Kapoor <69362333+kapooraryan@users.noreply.github.com>
2023-10-22 19:01:28 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ const About = () => {
       <div className="container mx-auto p-8 shadow-lg bg-gradient-to-br from-purple-400 to-indigo-600 font-karla text-white">
         <div className="flex flex-wrap items-center justify-center">
           <div className="w-full md:w-1/2 mb-6 md:mb-0">
-            <h1 className="text-4xl font-extrabold mb-4">About Our Inclusive Job Matching Platform</h1>
+            <h1 className="text-4xl font-extrabold mb-4">About Us: Empowering Differences, Enriching Businesses.</h1>
             <p className="text-lg">
               Welcome to EquiHire, where we are dedicated to reshaping the job market, creating a more inclusive space for individuals with disabilities.
             </p>
@@ -22,9 +22,11 @@ const About = () => {
               Our platform offers an array of features to ensure a seamless and inclusive job search experience.
 
               <ul className="list-disc list-inside mt-4">
-                <li>Text-to-Speech</li>
-                <li>Machine Learning Models</li>
-                <li>Direct Connection with Potential Employers</li>
+                <li>Connect with Potential Employers.</li>
+                <li>Voice recognition functionality.</li>
+                <li>Toggle Colour Blind Model.</li>
+                <li>Job Role Finder ML Model.</li>
+                <li>Data Security.</li>
               </ul>
             </p>
           </div>

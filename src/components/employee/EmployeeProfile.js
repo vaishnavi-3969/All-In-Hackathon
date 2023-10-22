0f0b9f0818e5e9008f8fc91fa142657e4a2ab067
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-10-22 15:21:34 +0530

    DF

--- src/components/employee/EmployeeProfile.js
+++ src/components/employee/EmployeeProfile.js
@@ -132,7 +132,7 @@ const BasicInfoSection = ({ profileData }) => {
                         </Field>
                         <ErrorMessage name="disability" component="div" className="text-red-500" />
                     </div>
-                    <button onClick={handleSubmit} type="submit" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+                    <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
                         Save
                     </button>
                 </Form>
